dfd4db3534084edfc32c64fb5360de7b3ef4e83a
jyothi3617 <jyothi.madduri18@gmail.com>
2022-12-26 22:22:58 -0800

    small

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>hello jyothi</p>
+        <p>hello jyothi</p>
         <a
           className="App-link"
           href="https://reactjs.org"
